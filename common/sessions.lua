d224c12f6c32fa833b95b7831eba009f83bc702b
gaoqingfeng <gaoqingfeng@bilibili.com>
2020-04-03 12:44:21 +0800

    add lua sessions

---
+++ common/sessions.lua
@@ -0,0 +1,36 @@
+local net = net;
+
+-- sessions manager.
+local LSessions = {
+
+}
+
+-- create session.
+function LSessions:create(session)
+    if self[session] then return end
+
+    -- get session info.
+    local ip, port = common.get_session_info(session);
+    local LSession = { -- new session.
+        session = session,
+        ip   = info.ip,
+        port = info.port,
+        Send = function(msgid, data)
+            net.client_send(self.sess, msgid, data, #data);
+        end
+    }
+    self[session] = LSession;
+    return LSession;
+end
+
+-- get session.
+function LSessions:get(session)
+    return self[session];
+end
+
+-- remove.
+function LSessions:remove(session)
+    self[session] = nil;
+end
+
+return sessions;
