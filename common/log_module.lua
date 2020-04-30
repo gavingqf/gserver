46fe52d45bebd686866a1d35cd6282680b922c73
gaoqingfeng <gaoqingfeng@bilibili.com>
2020-04-30 15:01:54 +0800

    log module optimize

--- common/log_module.lua
+++ common/log_module.lua
@@ -3,33 +3,46 @@
 -- asyncronous log and syncronous log(S)
 --
 local log = log;
+local table_concat = table.concat;
 
 -- log moudle.
 local LogModule = {
 
 };
 
-function LogModule:Crit(c)
+--- concat all ... string.
+local function concat(...)
+    local strs = {...};
+	return table_concat(strs);	
+end
+
+function LogModule:Crit(...)
+    local c = concat(...);
     log.crit(c);
 end
 
-function LogModule:SCrit(c)
+function LogModule:SCrit(...)
+    local c = concat(...);
     log.scrit(c);
 end
 
-function LogModule:Info(c)
+function LogModule:Info(...)
+    local c = concat(...);
     log.info(c);
 end
 
-function LogModule:SInfo(c)
+function LogModule:SInfo(...)
+    local c = concat(...);
     log.sinfo(c);
 end
 
-function LogModule:Warn(c)
+function LogModule:Warn(...)
+    local c = concat(...);
     log.warn(c);
 end
 
-function LogModule:SWarn(c)
+function LogModule:SWarn(...)
+    local c = concat(...);
     log.swarn(c);
 end
 
